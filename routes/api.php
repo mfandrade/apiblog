f61f08800d478d26f96f3462148bb3cd1251e4ec
Marcelo F Andrade <mfandrade@gmail.com>
2022-09-18 15:27:17 -0300

    /api health response

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,8 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function() {
+    return 'API is up and running...';
+});
 Route::apiResource('post', PostController::class);
 Route::apiResource('comment', CommentController::class);
