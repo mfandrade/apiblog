cc56ca83ca9408328a030dba1c67bbe8abbf4ce6
Marcelo F Andrade <mfandrade@gmail.com>
2022-09-19 15:25:51 -0300

    format sql

--- .infra/sql/3_insert_a_single_demo_comment.sql
+++ .infra/sql/3_insert_a_single_demo_comment.sql
@@ -1,8 +1,10 @@
-LOCK TABLES `comments` WRITE;
-INSERT INTO `comments`(
-  `post_id`,
-  `text`) VALUES (
+LOCK TABLES comments WRITE;
+INSERT INTO comments
+(
+  post_id, text
+) VALUES
+(
   2,
-  'Muito lindo esse soneto! Vinícius de Moraes é maravilhoso!');
+  'Muito lindo esse soneto!  Vinícius de Moraes é maravilhoso!'
+);
 UNLOCK TABLES;
-
