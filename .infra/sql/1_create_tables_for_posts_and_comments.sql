343363594d4347f91dabdc65a02a7c9477e4f7b3
Marcelo F Andrade <mfandrade@gmail.com>
2022-09-19 10:48:48 -0300

    simplify sql syntax

--- .infra/sql/1_create_tables_for_posts_and_comments.sql
+++ .infra/sql/1_create_tables_for_posts_and_comments.sql
@@ -1,21 +1,23 @@
-DROP TABLE IF EXISTS `posts`;
-CREATE TABLE `posts` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(140) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `body` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts
+(
+  id         INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  title      VARCHAR(140) NOT NULL,
+  body       TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE `comments` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `post_id` int unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `comments_post_id_foreign` (`post_id`),
-  CONSTRAINT `comments_post_id_foreign` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+DROP TABLE IF EXISTS comments;
+CREATE TABLE comments
+(
+  id         INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  text       TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  post_id    INT UNSIGNED NOT NULL,
+  FOREIGN KEY (post_id)
+      REFERENCES posts (id)
+      ON DELETE CASCADE
+) ENGINE=InnoDB;
+
