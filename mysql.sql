f33955a8bb4a7ed4fc5d5a4bf057093cde9da58e
Marcelo F Andrade <mfandrade@gmail.com>
2022-09-21 17:18:46 -0300

    project restructure

---
+++ mysql.sql
@@ -0,0 +1,72 @@
+-- BLANK FILE
+-- ----------------------------------------------------------------------
+-- CREATE POSTS AND COMMENTS TABLES
+-- ----------------------------------------------------------------------
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts
+(
+  id         INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  title      VARCHAR(140) NOT NULL,
+  body       TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS comments;
+CREATE TABLE comments
+(
+  id         INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  text       TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  post_id    INT UNSIGNED NOT NULL,
+  FOREIGN KEY (post_id)
+      REFERENCES posts (id)
+      ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- INSERT TWO LINES IN TABLE posts
+-- ----------------------------------------------------------------------
+LOCK TABLES posts WRITE;
+INSERT INTO posts
+(
+  id, title, body
+) VALUES
+(
+  1,
+  'Hello API World!',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+   tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim
+   veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+   commodo consequat.  Duis aute irure dolor in reprehenderit in voluptate
+   velit esse cillum dolore eu fugiat nulla pariatur.  Excepteur sint occaecat
+   cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+   est laborum.'
+);
+INSERT INTO posts
+(
+  title, body
+) VALUES
+( 'Soneto de Fidelidade',
+  '<p>De tudo ao meu amor serei atento <br>Antes; e com tal zelo, e sempre, e
+   tanto <br>Que mesmo em face do maior encanto <br>Dele se encante mais meu
+   pensamento</p><p>Quero vivê-lo em cada vão momento <br>E em seu louvor hei
+   de espalhar meu canto <br>E rir me riso, e derramar meu pranto <br>Ao seu
+   pesar ou seu contentamento</p><p>E assim quando mais tarde me procure
+   <br>quem sabe a morte, angústia de quem vive <br>quem sabe a solidão, fim
+   de quem ama</p><p>Eu possa me dizer do amor (que tive) <br>que não seja
+   imortal, posto que é chama <br>mas que seja infinito enquanto dure.</p>'
+);
+UNLOCK TABLES;
+-- INSERT A SINGLE DEMO LINE IN TABLE comments
+-- ----------------------------------------------------------------------
+LOCK TABLES comments WRITE;
+INSERT INTO comments
+(
+  post_id, text
+) VALUES
+(
+  2,
+  'Muito lindo esse soneto!  Vinícius de Moraes é maravilhoso!'
+);
+UNLOCK TABLES;
